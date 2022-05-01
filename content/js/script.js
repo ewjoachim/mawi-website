eb73e639ec6b22b9d52aa14204b47794841a0429
Joachim Jablon <ewjoachim@gmail.com>
2022-05-01 21:31:17 +0200

    Most content added

--- content/js/script.js
+++ content/js/script.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var mapData = JSON.parse(document.getElementById("map-data").innerText);
 
-    var map = L.map('map').setView([46.120, 1.051], 10);
+    var map = L.map(
+      'map',
+      {scrollWheelZoom: false}
+    ).setView(mapData.init, mapData.initZoom);
+
+    map.on('focus', function() { console.log("focus"); map.scrollWheelZoom.enable(); });
+    map.on('blur', function() { console.log("blur"); map.scrollWheelZoom.disable(); });
+
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 10,
+      maxZoom: 15,
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken: mapData.token
     }).addTo(map);
-      L.polygon([
-      [46.227, 0.920],
-      [46.054, 1.336],
-      [46.045, 0.843]
-    ]).addTo(map);
+      L.circle(mapData.center, mapData.radius).addTo(map);
 });
